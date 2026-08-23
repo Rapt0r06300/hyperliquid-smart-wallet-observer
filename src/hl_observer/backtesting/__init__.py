9c6be6903eebc3782bdb22be51c21f49c6dcb9f9
Rapt0r06300 <bermond.florent06@gmail.com>
2026-08-24 00:56:20 +0200

    Préserve l’alias causal legacy Lead-Lag

--- src/hl_observer/backtesting/__init__.py
+++ src/hl_observer/backtesting/__init__.py
@@ -5,11 +5,13 @@ from hl_observer.backtesting.experiment import (
     PriceTick,
     run_paper_replay_experiment,
 )
+from hl_observer.backtesting import lead_lag_causal_gap_compat as lead_lag_causal_gap_diagnostic
 
 __all__ = [
     "BacktestExperimentConfig",
     "BacktestExperimentResult",
     "BacktestReplayDecision",
     "PriceTick",
+    "lead_lag_causal_gap_diagnostic",
     "run_paper_replay_experiment",
 ]
