b13a4456184037406217e0a9fd887c6e0c55d8f6
Rapt0r06300 <bermond.florent06@gmail.com>
2026-09-13 22:32:53 +0200

    feat: export native multi-venue market data

--- src/hl_observer/collection/__init__.py
+++ src/hl_observer/collection/__init__.py
@@ -1,20 +1,11 @@
 """Read-only Hyperliquid collection pipeline."""
 
-from hl_observer.collection.collector import CollectionPlan, CollectionResult, run_collection_once
-from hl_observer.collection.weight_budgeter import (
-    BudgetPlan,
-    ReadOnlyBudgetRequest,
-    format_budget_plan,
-    plan_readonly_collection_budget,
-)
-from hl_observer.collection.proxy_pool import (
-    ProxyEndpoint,
-    ProxyPoolPlan,
-    ShardAssignment,
-    apply_proxy_health_event,
-    plan_sticky_proxy_shards,
-)
 from hl_observer.collection.backoff import BackoffDecision, BackoffPolicy, compute_backoff_delay
+from hl_observer.collection.bybit_market_data import (
+    BybitMarketState,
+    BybitPublicClient,
+    parse_bybit_linear_instruments,
+)
 from hl_observer.collection.circuit_breaker import (
     CircuitBreakerConfig,
     CircuitBreakerState,
@@ -23,7 +14,31 @@ from hl_observer.collection.circuit_breaker import (
     record_failure,
     record_success,
 )
+from hl_observer.collection.collector import CollectionPlan, CollectionResult, run_collection_once
 from hl_observer.collection.html_scraper import HtmlScrapeResult, HtmlWalletCandidate, parse_wallets_from_html
+from hl_observer.collection.native_venue_coordinator import NativeVenueCoordinator
+from hl_observer.collection.native_venue_market import (
+    DESYNC,
+    EXPLOITABLE,
+    STALE,
+    UNMEASURABLE,
+    MarketLevel,
+    MultiVenueMarketStore,
+    NativeMarketSnapshot,
+    canonical_coin,
+)
+from hl_observer.collection.okx_market_data import (
+    OkxMarketState,
+    OkxPublicClient,
+    parse_okx_swap_instruments,
+)
+from hl_observer.collection.proxy_pool import (
+    ProxyEndpoint,
+    ProxyPoolPlan,
+    ShardAssignment,
+    apply_proxy_health_event,
+    plan_sticky_proxy_shards,
+)
 from hl_observer.collection.public_fetcher import (
     MemoryFetchCache,
     PublicFetchRequest,
@@ -31,34 +46,55 @@ from hl_observer.collection.public_fetcher import (
     fetch_public_page,
 )
 from hl_observer.collection.rate_limiter import WindowRateLimiter
+from hl_observer.collection.weight_budgeter import (
+    BudgetPlan,
+    ReadOnlyBudgetRequest,
+    format_budget_plan,
+    plan_readonly_collection_budget,
+)
 
 __all__ = [
-    "BudgetPlan",
-    "CollectionPlan",
-    "CollectionResult",
     "BackoffDecision",
     "BackoffPolicy",
+    "BudgetPlan",
+    "BybitMarketState",
+    "BybitPublicClient",
     "CircuitBreakerConfig",
     "CircuitBreakerState",
+    "CollectionPlan",
+    "CollectionResult",
+    "DESYNC",
+    "EXPLOITABLE",
     "HtmlScrapeResult",
     "HtmlWalletCandidate",
+    "MarketLevel",
     "MemoryFetchCache",
+    "MultiVenueMarketStore",
+    "NativeMarketSnapshot",
+    "NativeVenueCoordinator",
+    "OkxMarketState",
+    "OkxPublicClient",
     "ProxyEndpoint",
     "ProxyPoolPlan",
     "PublicFetchRequest",
     "PublicFetchResult",
     "ReadOnlyBudgetRequest",
+    "STALE",
     "ShardAssignment",
+    "UNMEASURABLE",
     "WindowRateLimiter",
     "apply_proxy_health_event",
     "can_attempt",
+    "canonical_coin",
     "compute_backoff_delay",
-    "format_budget_plan",
     "fetch_public_page",
+    "format_budget_plan",
     "maybe_half_open",
+    "parse_bybit_linear_instruments",
+    "parse_okx_swap_instruments",
     "parse_wallets_from_html",
-    "plan_sticky_proxy_shards",
     "plan_readonly_collection_budget",
+    "plan_sticky_proxy_shards",
     "record_failure",
     "record_success",
     "run_collection_once",
